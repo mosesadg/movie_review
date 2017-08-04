0b73e3037ef09bc14b5bd4eadcbd119399a6f558
Anand <moses.adg@gmail.com>
2017-08-03 20:33:19 -0400

    nil guard

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,7 +17,7 @@ class MoviesController < ApplicationController
     if @reviews.blank?
       @avg_review = 0
     else
-      @avg_review = @reviews.average(:rating).round(2)
+      @avg_review = @reviews.average(:rating).round(2) if @avg_review
       end
   end
 
